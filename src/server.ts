ca8935f5c2450084b7211232a06bdf410e5a843a
Tom Marien <tommarien@gmail.com>
2018-02-16 23:36:04 +0100

    Small refactoring

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import * as config from './config/config';
 const port = config.runtime.port;
 
 const server = app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port} in ${app.get('env')} mode`);
+  console.log(`Server is listening at http://localhost:${port} in ${config.runtime.env} mode`);
   console.log('Press CTRL-C to stop\n');
 });
 
