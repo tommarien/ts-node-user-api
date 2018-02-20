a91861eec15aac7d8d06b2b003b7a12235d8578b
Tom Marien <tommarien@gmail.com>
2018-02-20 09:30:24 +0100

    Refactor sigterm handling

--- src/server.ts
+++ src/server.ts
@@ -13,17 +13,19 @@ mongoDb()
       log.info('Started', { port, env });
       log.debug('Press CTRL-C to stop');
 
+      const handleSignal = (signal: string) => {
+        log.info(`Stopping server due to ${signal} signal`);
+        // This makes sure no new request come in.
+        server.close(() => {
+          process.exit(0);
+        });
+      };
+
       /*
       * Heroku is sending a SIGTERM when an application is restarted or a downscale event happened
       * We want to make sure that we don't kill the process as a result of that.
       * After 30s heroku will send a SIGKILL, which will terminate the process.
       */
-      process.on('SIGTERM', () => {
-        log.debug('Stopping server due to SIGTERM signal');
-        // This makes sure no new request come in.
-        server.close(() => {
-          process.exit(0);
-        });
-      });
+      process.on('SIGTERM', () => handleSignal('SIGTERM'));
     });
   });
